49a0e5e31c151fcef34a5275c3cfacc2c7bf1296
PatriciaDivi <patriciavianad@gmail.com>
2022-11-15 19:09:26 -0300

    feat(Development): :sparkles: Implementada função 'search_by_date'
    para filtra informações através da chave 'timestamp'

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,21 +1,34 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
     resultNews = list()
-    newNews = search_news({
-        # "$options": "i" = Procura por maiúsculas e minúsculas
-        "title": {"$regex": title, "$options": "i"},
-    })
+    newNews = search_news(
+        {
+            # "$options": "i" = Procura por maiúsculas e minúsculas
+            "title": {"$regex": title, "$options": "i"},
+        }
+    )
     for each_line in newNews:
         resultNews.append((each_line["title"], each_line["url"]))
     return resultNews
 
 
 # Requisito 7
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(dateParams):
+    try:
+        correctFormat = datetime.strptime(dateParams, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+        filterByDate = search_news({"timestamp": {"$regex": correctFormat}})
+        resultNews = list()
+        for each_line in filterByDate:
+            resultNews.append((each_line["title"], each_line["url"]))
+        return resultNews
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
